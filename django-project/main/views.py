30e2e11ef379d88f439c398d0e6651b584769aa8
raushanhan <raushanhan2808@icloud.com>
2026-06-30 00:06:53 +0300

    добавлена передача списка учителей в контекст

--- django-project/main/views.py
+++ django-project/main/views.py
@@ -4,20 +4,24 @@ from django.http import Http404
 from django.http import HttpResponse
 
 COURSES = {
-    "Python": "Здесь изучаем питон",
-    "plusy": "Здесь изучаем плюсы",
-    "Super_course": "Здесь изучаем что такое супер",
-    "Tatar_language": "учим татарский"
+    # "Python": "Здесь изучаем питон",
+    # "plusy": "Здесь изучаем плюсы",
+    # "Super_course": "Здесь изучаем что такое супер",
+    # "Tatar_language": "учим татарский"
+}
+
+TEACHER = {
+    'name': 'raushan'
 }
 
 def main_page(request):
 
-    return render(request, './base.html', context={'courses': list(COURSES.items())})
+    return render(request, './base.html', context={
+        'courses': list(COURSES.items()),
+        'teacher': TEACHER
+    })
 
 def course_description(request, course_name):
-
-    # if course_name not in COURSES:
-    #     raise Http404(f"курса {course_name} нет")
 
     if course_name not in COURSES:
         return HttpResponse(
