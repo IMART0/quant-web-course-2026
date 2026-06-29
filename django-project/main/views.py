958c96a09e4a35a588e8e707490caf202d2ca65a
raushanhan <raushanhan2808@icloud.com>
2026-06-29 09:10:31 +0300

    новая директория для дальнейшей работы над сайтом

---
+++ django-project/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import HttpResponse
+
+def main_page(request):
+    return HttpResponse("Добро пожаловать на официальный сайт лагеря Квант!")
+
+
